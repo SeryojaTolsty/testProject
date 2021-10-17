fd763f22cb574a45700e51fc5d1085dda4a010c8
Ser_Yo_Ja <seroja.92@mail.ru>
2021-10-18 01:19:21 +0300

    Initial commit

---
+++ src/main/java/NewPackage/Main.java
@@ -0,0 +1,46 @@
+package NewPackage;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //leapYear();
+        consoleInput();
+    }
+
+    /*private static void leapYear() {
+        int a = 2080;
+        if (a % 4 != 0) {
+            if (a % 100 != 0) {
+                if (a % 400 != 0){
+                    System.out.println("Год не високосный");
+                }
+            }
+        } else {
+            System.out.println("Год високосный");
+        }
+    }*/
+
+    private static void consoleInput() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите год: ");
+        int a = in.nextInt();
+        if (a % 4 != 0) {
+            if (a % 100 != 0) {
+                if (a % 400 != 0){
+                    System.out.println("Год не високосный");
+                }
+            }
+        } else {
+            System.out.println("Год високосный");
+        }
+
+
+
+
+    }
+
+
+
+
+}
